3fb2e4b2afcea2191778cc3a77e3ba29dcefa0e5
Tom Goldsmith <goldsmith.tee@gmail.com>
2021-01-07 09:42:52 -0500

    Upversion clojure project.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject react-native-init-shadow "0.1.9"
+(defproject react-native-init-shadow "0.1.10"
   :license {:name "MIT"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
